8912d677c1638f80f6dc83392b381a90421df931
Julien <julien.le.coupanec@gmail.com>
2019-02-25 16:36:12 +0100

    feat(languages): add country key

--- data/languages.js
+++ data/languages.js
@@ -1,726 +1,907 @@
 export default [
   {
     code: "aa",
+    country: "aa",
     name: "Afar"
   },
   {
     code: "ab",
+    country: "ab",
     name: "Abkhazian"
   },
   {
     code: "af",
+    country: "za",
     name: "Afrikaans"
   },
   {
     code: "ak",
+    country: "ak",
     name: "Akan"
   },
   {
     code: "am",
+    country: "et",
     name: "Amharic"
   },
   {
     code: "an",
+    country: "an",
     name: "Aragonese"
   },
   {
     code: "ar",
+    country: "ae",
     name: "Arabic"
   },
   {
     code: "as",
+    country: "in",
     name: "Assamese"
   },
   {
     code: "av",
+    country: "av",
     name: "Avar"
   },
   {
     code: "ay",
+    country: "ay",
     name: "Aymara"
   },
   {
     code: "az",
+    country: "az",
     name: "Azerbaijani"
   },
   {
     code: "ba",
+    country: "ru",
     name: "Bashkir"
   },
   {
     code: "be",
+    country: "by",
     name: "Belarusian"
   },
   {
     code: "bg",
+    country: "bg",
     name: "Bulgarian"
   },
   {
     code: "bh",
+    country: "bh",
     name: "Bihari"
   },
   {
     code: "bi",
+    country: "bi",
     name: "Bislama"
   },
   {
     code: "bm",
+    country: "bm",
     name: "Bambara"
   },
   {
     code: "bn",
+    country: "bd",
     name: "Bengali"
   },
   {
     code: "bo",
+    country: "cn",
     name: "Tibetan"
   },
   {
     code: "br",
+    country: "fr",
     name: "Breton"
   },
   {
     code: "bs",
+    country: "bs",
     name: "Bosnian"
   },
   {
     code: "ca",
+    country: "es",
     name: "Catalan"
   },
   {
     code: "ce",
+    country: "ce",
     name: "Chechen"
   },
   {
     code: "ch",
+    country: "ch",
     name: "Chamorro"
   },
   {
     code: "co",
+    country: "fr",
     name: "Corsican"
   },
   {
     code: "cr",
+    country: "cr",
     name: "Cree"
   },
   {
     code: "cs",
+    country: "cz",
     name: "Czech"
   },
   {
     code: "cu",
+    country: "cu",
     name: "Old Church Slavonic / Old Bulgarian"
   },
   {
     code: "cv",
+    country: "cv",
     name: "Chuvash"
   },
   {
     code: "cy",
+    country: "gb",
     name: "Welsh"
   },
   {
     code: "da",
+    country: "dk",
     name: "Danish"
   },
   {
     code: "de",
+    country: "de",
     name: "German"
   },
   {
     code: "dv",
+    country: "mv",
     name: "Divehi"
   },
   {
     code: "dz",
+    country: "dz",
     name: "Dzongkha"
   },
   {
     code: "ee",
+    country: "ee",
     name: "Ewe"
   },
   {
     code: "el",
+    country: "gr",
     name: "Greek"
   },
   {
     code: "en",
+    country: "gb",
     name: "English"
   },
   {
     code: "eo",
+    country: "eo",
     name: "Esperanto"
   },
   {
     code: "es",
+    country: "es",
     name: "Spanish"
   },
   {
     code: "et",
+    country: "ee",
     name: "Estonian"
   },
   {
     code: "eu",
+    country: "es",
     name: "Basque"
   },
   {
     code: "fa",
+    country: "ir",
     name: "Persian"
   },
   {
     code: "ff",
+    country: "ff",
     name: "Peul"
   },
   {
     code: "fi",
+    country: "fi",
     name: "Finnish"
   },
   {
     code: "fj",
+    country: "fj",
     name: "Fijian"
   },
   {
     code: "fo",
+    country: "fo",
     name: "Faroese"
   },
   {
     code: "fr",
+    country: "fr",
     name: "French"
   },
   {
     code: "fy",
+    country: "nl",
     name: "West Frisian"
   },
   {
     code: "ga",
+    country: "ie",
     name: "Irish"
   },
   {
     code: "gd",
+    country: "gb",
     name: "Scottish Gaelic"
   },
   {
     code: "gl",
+    country: "es",
     name: "Galician"
   },
   {
     code: "gn",
+    country: "gn",
     name: "Guarani"
   },
   {
     code: "gu",
+    country: "in",
     name: "Gujarati"
   },
   {
     code: "gv",
+    country: "gv",
     name: "Manx"
   },
   {
     code: "ha",
+    country: "ha",
     name: "Hausa"
   },
   {
     code: "he",
+    country: "il",
     name: "Hebrew"
   },
   {
     code: "hi",
+    country: "in",
     name: "Hindi"
   },
   {
     code: "ho",
+    country: "ho",
     name: "Hiri Motu"
   },
   {
     code: "hr",
+    country: "hr",
     name: "Croatian"
   },
   {
     code: "ht",
+    country: "ht",
     name: "Haitian"
   },
   {
     code: "hu",
+    country: "hu",
     name: "Hungarian"
   },
   {
     code: "hy",
+    country: "am",
     name: "Armenian"
   },
   {
     code: "hz",
+    country: "hz",
     name: "Herero"
   },
   {
     code: "ia",
+    country: "ia",
     name: "Interlingua"
   },
   {
     code: "id",
+    country: "id",
     name: "Indonesian"
   },
   {
     code: "ie",
+    country: "ie",
     name: "Interlingue"
   },
   {
     code: "ig",
+    country: "ng",
     name: "Igbo"
   },
   {
     code: "ii",
+    country: "cn",
     name: "Sichuan Yi"
   },
   {
     code: "ik",
+    country: "ik",
     name: "Inupiak"
   },
   {
     code: "io",
+    country: "io",
     name: "Ido"
   },
   {
     code: "is",
+    country: "is",
     name: "Icelandic"
   },
   {
     code: "it",
+    country: "it",
     name: "Italian"
   },
   {
     code: "iu",
+    country: "iu",
     name: "Inuktitut"
   },
   {
     code: "ja",
+    country: "jp",
     name: "Japanese"
   },
   {
     code: "jv",
+    country: "jv",
     name: "Javanese"
   },
   {
     code: "ka",
+    country: "ge",
     name: "Georgian"
   },
   {
     code: "kg",
+    country: "kg",
     name: "Kongo"
   },
   {
     code: "ki",
+    country: "ki",
     name: "Kikuyu"
   },
   {
     code: "kj",
+    country: "kj",
     name: "Kuanyama"
   },
   {
     code: "kk",
+    country: "kz",
     name: "Kazakh"
   },
   {
     code: "kl",
+    country: "gl",
     name: "Greenlandic"
   },
   {
     code: "km",
+    country: "kh",
     name: "Cambodian"
   },
   {
     code: "kn",
+    country: "in",
     name: "Kannada"
   },
   {
     code: "ko",
+    country: "kr",
     name: "Korean"
   },
   {
     code: "kr",
+    country: "kr",
     name: "Kanuri"
   },
   {
     code: "ks",
+    country: "ks",
     name: "Kashmiri"
   },
   {
     code: "ku",
+    country: "ku",
     name: "Kurdish"
   },
   {
     code: "kv",
+    country: "kv",
     name: "Komi"
   },
   {
     code: "kw",
+    country: "kw",
     name: "Cornish"
   },
   {
     code: "ky",
+    country: "kg",
     name: "Kirghiz"
   },
   {
     code: "la",
+    country: "la",
     name: "Latin"
   },
   {
     code: "lb",
+    country: "lu",
     name: "Luxembourgish"
   },
   {
     code: "lg",
+    country: "lg",
     name: "Ganda"
   },
   {
     code: "li",
+    country: "li",
     name: "Limburgian"
   },
   {
     code: "ln",
+    country: "ln",
     name: "Lingala"
   },
   {
     code: "lo",
+    country: "la",
     name: "Laotian"
   },
   {
     code: "lt",
+    country: "lt",
     name: "Lithuanian"
   },
   {
     code: "lv",
+    country: "lv",
     name: "Latvian"
   },
   {
     code: "mg",
+    country: "mg",
     name: "Malagasy"
   },
   {
     code: "mh",
+    country: "mh",
     name: "Marshallese"
   },
   {
     code: "mi",
+    country: "nz",
     name: "Maori"
   },
   {
     code: "mk",
+    country: "mk",
     name: "Macedonian"
   },
   {
     code: "ml",
+    country: "in",
     name: "Malayalam"
   },
   {
     code: "mn",
+    country: "mn",
     name: "Mongolian"
   },
   {
     code: "mo",
+    country: "mo",
     name: "Moldovan"
   },
   {
     code: "mr",
+    country: "in",
     name: "Marathi"
   },
   {
     code: "ms",
+    country: "bn",
     name: "Malay"
   },
   {
     code: "mt",
+    country: "mt",
     name: "Maltese"
   },
   {
     code: "my",
+    country: "my",
     name: "Burmese"
   },
   {
     code: "na",
+    country: "na",
     name: "Nauruan"
   },
   {
     code: "nd",
+    country: "nd",
     name: "North Ndebele"
   },
   {
     code: "ne",
+    country: "np",
     name: "Nepali"
   },
   {
     code: "ng",
+    country: "ng",
     name: "Ndonga"
   },
   {
     code: "nl",
+    country: "nl",
     name: "Dutch"
   },
   {
     code: "nn",
+    country: "no",
     name: "Norwegian Nynorsk"
   },
   {
     code: "no",
+    country: "no",
     name: "Norwegian"
   },
   {
     code: "nr",
+    country: "nr",
     name: "South Ndebele"
   },
   {
     code: "nv",
+    country: "nv",
     name: "Navajo"
   },
   {
     code: "ny",
+    country: "ny",
     name: "Chichewa"
   },
   {
     code: "oc",
+    country: "fr",
     name: "Occitan"
   },
   {
     code: "oj",
+    country: "oj",
     name: "Ojibwa"
   },
   {
     code: "om",
+    country: "om",
     name: "Oromo"
   },
   {
     code: "or",
+    country: "in",
     name: "Oriya"
   },
   {
     code: "os",
+    country: "os",
     name: "Ossetian"
   },
   {
     code: "pa",
+    country: "in",
     name: "Punjabi"
   },
   {
     code: "pi",
+    country: "pi",
     name: "Pali"
   },
   {
     code: "pl",
+    country: "pl",
     name: "Polish"
   },
   {
     code: "ps",
+    country: "af",
     name: "Pashto"
   },
   {
     code: "pt",
+    country: "pt",
     name: "Portuguese"
   },
   {
     code: "qu",
+    country: "qu",
     name: "Quechua"
   },
   {
     code: "rm",
+    country: "ch",
     name: "Raeto Romance"
   },
   {
     code: "rn",
+    country: "rn",
     name: "Kirundi"
   },
   {
     code: "ro",
+    country: "ro",
     name: "Romanian"
   },
   {
     code: "ru",
+    country: "ru",
     name: "Russian"
   },
   {
     code: "rw",
+    country: "rw",
     name: "Rwandi"
   },
   {
     code: "sa",
+    country: "in",
     name: "Sanskrit"
   },
   {
     code: "sc",
+    country: "sc",
     name: "Sardinian"
   },
   {
     code: "sd",
+    country: "sd",
     name: "Sindhi"
   },
   {
     code: "sg",
+    country: "sg",
     name: "Sango"
   },
   {
     code: "sh",
+    country: "sh",
     name: "Serbo-Croatian"
   },
   {
     code: "si",
+    country: "lk",
     name: "Sinhalese"
   },
   {
     code: "sk",
+    country: "sk",
     name: "Slovak"
   },
   {
     code: "sl",
+    country: "si",
     name: "Slovenian"
   },
   {
     code: "sm",
+    country: "sm",
     name: "Samoan"
   },
   {
     code: "sn",
+    country: "sn",
     name: "Shona"
   },
   {
     code: "so",
+    country: "so",
     name: "Somalia"
   },
   {
     code: "sq",
+    country: "al",
     name: "Albanian"
   },
   {
     code: "sr",
+    country: "sr",
     name: "Serbian"
   },
   {
     code: "ss",
+    country: "ss",
     name: "Swati"
   },
   {
     code: "st",
+    country: "st",
     name: "Southern Sotho"
   },
   {
     code: "su",
+    country: "su",
     name: "Sundanese"
   },
   {
     code: "sv",
+    country: "se",
     name: "Swedish"
   },
   {
     code: "sw",
+    country: "ke",
     name: "Swahili"
   },
   {
     code: "ta",
+    country: "in",
     name: "Tamil"
   },
   {
     code: "te",
+    country: "in",
     name: "Telugu"
   },
   {
     code: "tg",
+    country: "tg",
     name: "Tajik"
   },
   {
     code: "th",
+    country: "th",
     name: "Thai"
   },
   {
     code: "ti",
+    country: "ti",
     name: "Tigrinya"
   },
   {
     code: "tk",
+    country: "tm",
     name: "Turkmen"
   },
   {
     code: "tl",
+    country: "tl",
     name: "Tagalog"
   },
   {
     code: "tn",
+    country: "za",
     name: "Tswana"
   },
   {
     code: "to",
+    country: "to",
     name: "Tonga"
   },
   {
     code: "tr",
+    country: "tr",
     name: "Turkish"
   },
   {
     code: "ts",
+    country: "ts",
     name: "Tsonga"
   },
   {
     code: "tt",
+    country: "ru",
     name: "Tatar"
   },
   {
     code: "tw",
+    country: "tw",
     name: "Twi"
   },
   {
     code: "ty",
+    country: "ty",
     name: "Tahitian"
   },
   {
     code: "ug",
+    country: "cn",
     name: "Uyghur"
   },
   {
     code: "uk",
+    country: "ua",
     name: "Ukrainian"
   },
   {
     code: "ur",
+    country: "pk",
     name: "Urdu"
   },
   {
     code: "ve",
+    country: "ve",
     name: "Venda"
   },
   {
     code: "vi",
+    country: "vn",
     name: "Vietnamese"
   },
   {
     code: "vo",
+    country: "vo",
     name: "Volapük"
   },
   {
     code: "wa",
+    country: "wa",
     name: "Walloon"
   },
   {
     code: "wo",
+    country: "sn",
     name: "Wolof"
   },
   {
     code: "xh",
+    country: "za",
     name: "Xhosa"
   },
   {
     code: "yi",
+    country: "yi",
     name: "Yiddish"
   },
   {
     code: "yo",
+    country: "ng",
     name: "Yoruba"
   },
   {
     code: "za",
+    country: "za",
     name: "Zhuang"
   },
   {
     code: "zh",
+    country: "cn",
     name: "Chinese"
   },
   {
     code: "zu",
+    country: "za",
     name: "Zulu"
   }
 ];
