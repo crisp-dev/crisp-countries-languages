9c7e84827178328b9bcd81bbf7b723bb5e524a90
Julien <julien.le.coupanec@gmail.com>
2019-02-25 15:30:01 +0100

    feat(app): add getLanguageCode and getLanguageName

--- index.js
+++ index.js
@@ -6,6 +6,26 @@ import dataForCountries from "./data/countries.js";
 import dataForLanguages from "./data/languages.js";
 
 /**************************************************************************
+ * FUNCTIONS
+ ***************************************************************************/
+
+var languageNameMap = {};
+var languageCodeMap = {};
+
+dataForLanguages.forEach(function(language) {
+  languageCodeMap[language.code.toLowerCase()] = language.name;
+  languageNameMap[language.name.toLowerCase()] = language.code;
+});
+
+export const getLanguageCode = function getLanguageCode(name) {
+  return languageNameMap[name.toLowerCase()];
+};
+
+export const getLanguageName = function getLanguageNames(code) {
+  return languageCodeMap[code.toLowerCase()];
+};
+
+/**************************************************************************
  * EXPORTS
  ***************************************************************************/
 
