59aa0ad0361b70f6c506aed1ac4b2e182cc90d10
Valerian Saliou <valerian@valeriansaliou.name>
2023-07-18 22:04:48 +0200

    Fix TS

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,28 +1,28 @@
-declare namespace CrispCountriesLanguages {
-  interface Country {
-    code: string;
-    name: string;
-  }
-
-  interface Language {
-    code: string;
-    country: string;
-    name: string;
-    direction: string;
-  }
-
-  type Countries = { [code: string]: Country };
-  type Languages = { [code: string]: Language };
-
-  function getCountryCode(name: string): string | void;
-  function getCountryName(code: string): string | void;
-  function getLanguageCode(name: string): string | void;
-  function getLanguageCountry(code: string): string | void;
-  function getLanguageName(code: string): string | void;
-  function getLanguageDirection(code: string): string | void;
-
-  const countries: Countries;
-  const languages: Languages;
+export interface Country {
+  code: string;
+  name: string;
 }
 
-export as namespace CrispCountriesLanguages;
+export interface Language {
+  code: string;
+  country: string;
+  name: string;
+  direction: string;
+}
+
+export function getCountryCode(name: string): string | void;
+export function getCountryName(code: string): string | void;
+export function getLanguageCode(name: string): string | void;
+export function getLanguageCountry(code: string): string | void;
+export function getLanguageName(code: string): string | void;
+export function getLanguageDirection(code: string): string | void;
+
+export const countries: Array<Country>;
+export const languages: Array<Language>;
+
+declare const crispCountriesLanguages: {
+  dataForCountries: Array<Country>;
+  dataForLanguages: Array<Language>;
+};
+
+export = crispCountriesLanguages;
